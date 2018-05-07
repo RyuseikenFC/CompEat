49c140e1a6eb85f462d07b5b647b4659d551c799
Steven <calmrainbp@gmail.com>
2018-05-06 21:00:17 -0700

    final product. lost to 2 knock offs that were rosetta stones speech to text accent corrector, and targets and wallmarts cartwheel shopping app. start up eastbay 2018

--- CompEat/View/Challange1VC.swift
+++ CompEat/View/Challange1VC.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class Challange1VC: UIViewController {
 
+    
+    var seconds = 20
+    var timer = Timer()
+    
     @IBOutlet weak var imgImage: UIImageView!
+    @IBOutlet var complete: UILabel!
+    @IBOutlet weak var miles: UILabel!
+    @IBOutlet weak var start_challange: UIImageView!
+    //@IBOutlet weak var dog2: UIImageView!
+    
     
     override func viewDidLoad() {
+        complete.isHidden = true
+       // dog2.isHidden = true
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
@@ -25,9 +36,43 @@ class Challange1VC: UIViewController {
     
     
     @IBAction func startWalk(_ sender: Any) {
-        UIView.animate(withDuration: 7, animations: {
-            self.imgImage.frame.origin.x += 200
+        UIView.animate(withDuration: 10, animations: {
+
+            self.imgImage.frame.origin.x += 110
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Challange1VC.counter), userInfo: nil, repeats: true)
         }, completion: nil)
+        //self.dog2.frame.origin.x  = self.imgImage.frame.origin.x
+       // self.dog2.frame.origin.y  = self.imgImage.frame.origin.y
+        UIView.animate(withDuration: 10, animations: {
+            
+            self.imgImage.frame.origin.x += 90
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Challange1VC.counter), userInfo: nil, repeats: true)
+        }, completion: nil)
+    }
+    
+    @objc func counter(){
+        seconds -= 1
+        miles.text = String(seconds) + " miles left"
+        
+        if(seconds == 1){
+            miles.text = String(seconds) + " mile left"
+        }
+        
+        if(seconds == 0){
+            timer.invalidate()
+            swap()
+        }
+    }
+    
+    @IBAction func test(_ sender: Any){
+        complete.isHidden = false
+        miles.isHidden = true
+    }
+    
+    func swap(){
+        complete.isHidden = false
+        miles.isHidden = true
+        start_challange.isHidden = true
     }
     /*
     // MARK: - Navigation
